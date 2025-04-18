5f988c834fd6f7d8773bd8fe55bd00bd6267d829
Demid <myemail@foobar.com>
2025-04-19 00:14:11 +0300

    feat(models): add save_to_redis method for Player

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,8 +1,12 @@
 import uuid
+from typing import Literal, Annotated
 
+from fastapi import Depends
 from sqlmodel import Field, Relationship, SQLModel
+from redis import Redis
 from redis_om import HashModel
 
+from app.core.database import get_redis_connection
 
 class UserCreate(SQLModel):
     """User model to receive via API on creation."""
@@ -29,7 +33,8 @@ class Lobby(LobbyCreate):
     A group of players joins together in lobby. Lobby always has at least one player (`creator`)."""
 
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    players: list["Player"] = Relationship(back_populates="lobby")
+    player_ids: list[uuid.UUID] = Relationship(back_populates="lobby")
+    status: Literal["waiting", "playing", "finishing"] = "waiting"
 
 
 class Player(User):
@@ -38,4 +43,11 @@ class Player(User):
     Player is the user who is in lobby."""
 
     lobby_id: int | None = Field(default=None, foreign_key="lobby.id")
-    lobby: Lobby | None = Relationship(back_populates="players")
+    lobby: Lobby | None = Relationship(back_populates="player_ids")
+
+    def save_to_redis(self, redis_connection: Annotated[Redis[str], Depends[get_redis_connection]]) -> str:
+        """Save player to redis DB and return key."""
+        data = self.model_dump()
+        redis_key = f"player:{self.id}"
+        redis_connection.hset(redis_key, mapping=data)
+        return redis_key
