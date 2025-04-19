2efd9d9781145f470d3729d5000f13fb88219364
Demid <myemail@foobar.com>
2025-04-19 17:55:12 +0300

    feat(deps.py): add redis connection dependency

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,7 +1,10 @@
 from typing import Annotated
 from fastapi import Depends
+from redis import Redis
 from sqlmodel import Session
 
-from app.core.database import get_pg_session
+from app.core.database import get_pg_session, get_redis_connection
 
-SessionDep = Annotated[Session, Depends(get_pg_session)]
+
+PgSessionDep = Annotated[Session, Depends(get_pg_session)]
+RedisConnectionDep = Annotated[Redis[str], Depends(get_redis_connection)]
