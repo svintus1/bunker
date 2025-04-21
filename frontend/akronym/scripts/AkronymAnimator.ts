bdc714e2edd7664f036188f991868e76d114c7aa
svintus1 <153781107+svintus1@users.noreply.github.com>
2025-04-22 01:27:27 +0300

    refactor(css, ts): refactor of code (#15)
    * feat(sound): add music and music-toggle
    
    * ui(backdrop-menu): add backdrop with blur for menu elements
    
    * hui
    
    * pizda
    
    * refactor(css, ts): refactor of code
    
    feat(ui): add modal window for lobby ID
    
    style(ui): add animations for menu elements
    
    ---------
    
    Co-authored-by: cringen33r <cringen33r@yandex.ru>

--- frontend/akronym/scripts/AkronymAnimator.ts
+++ frontend/akronym/scripts/AkronymAnimator.ts
@@ -1,58 +1,61 @@
 import { CASCADE_ANIMATION_DELAY, AkronymState, AkronymAnimation, AkronymVisibility } from './AkronymGlobal.js';
 
 export class AkronymAnimator {
-  static changeVisibility(element: any, visibility: AkronymVisibility, animation: AkronymAnimation, duration: number, isCascade: boolean = false): void {
+  static changeVisibility(
+    element: any,
+    visibility: AkronymVisibility,
+    animation: AkronymAnimation,
+    duration: number = 0,
+    delay: number = 0,
+    isCascade: boolean = false,
+    cascadeAnimation: AkronymAnimation = 'none',
+    cascadeAnimationDuration: number = 0
+  ): void {
     const current = element.dataset.visibility ?? 'visible';
-    if (current === visibility) return;
-
+    if (current === visibility){
+      return
+    };
+  
     element.dataset.visibility = visibility;
-
-    const cascadeIndex = parseInt(
-      visibility === 'visible'
-        ? element.dataset.cascadeShow ?? '0'
-        : element.dataset.cascadeHide ?? '0'
-    );
-
-    // Запуск анимации для текущего элемента
-    setTimeout(() => {
-        AkronymAnimator.runAnimation(element, animation, duration, visibility);
-    }, cascadeIndex * CASCADE_ANIMATION_DELAY);
-
-    // Если isCascade включен, рекурсивно вызываем функцию для дочерних элементов
+  
+    AkronymAnimator.runAnimation(element, animation, duration, delay, visibility);
+  
     if (isCascade) {
-        const children = element.querySelectorAll('[data-cascade-show], [data-cascade-hide]');
-        
-        children.forEach((child: any) => {
-            // Получаем каскадный индекс для каждого дочернего элемента
-            const childCascadeIndex = parseInt(
-                visibility === 'visible'
-                    ? child.dataset.cascadeShow ?? '0'
-                    : child.dataset.cascadeHide ?? '0'
-            );
-
-            // Устанавливаем задержку с учётом индивидуального индекса дочернего элемента
-            setTimeout(() => {
-                AkronymAnimator.changeVisibility(child, visibility, animation, duration, false);
-            }, (cascadeIndex + childCascadeIndex) * CASCADE_ANIMATION_DELAY); // Увеличиваем задержку с учётом индекса дочернего элемента
-        });
+      const children = Array.from(element.children);
+  
+      children.forEach((child: any, index: number) => {
+        setTimeout(() => {
+          AkronymAnimator.changeVisibility(
+            child,
+            visibility,
+            cascadeAnimation,
+            cascadeAnimationDuration,
+            delay,
+            false
+          );
+        }, index * CASCADE_ANIMATION_DELAY);
+      });
     }
-}
+  }
 
-
-  static changeState(element: any, state: AkronymState, animation: AkronymAnimation, duration: number): void {
+  static changeState(element: any, state: AkronymState, animation: AkronymAnimation, duration: number, delay: number): void {
     const current = element.dataset.state ?? 'idle';
     if (current === state) return;
 
     element.dataset.state = state;
 
-    AkronymAnimator.runAnimation(element, animation, duration, state);
+    AkronymAnimator.runAnimation(element, animation, duration, delay, state);
   }
 
-  static runAnimation(element: any, animation: AkronymAnimation, duration: number, target: AkronymState | AkronymVisibility): void {
+  static runAnimation(element: any, animation: AkronymAnimation, duration: number, delay: number, target: AkronymState | AkronymVisibility): void {
     element.dataset.animation = animation;
-    if (element.dataset.visibility == 'visible'){
+    element.style.animationDuration = duration + 'ms';
+    setTimeout(() => {
+      if (element.dataset.visibility == 'visible'){
+        element.style.animationPlayState = 'running';
         element.removeAttribute('data-hidden');
-    }
+      }
+    }, delay);
 
     setTimeout(() => {
       element.dataset.animation = 'none';
@@ -61,6 +64,6 @@ export class AkronymAnimator {
       }
       element.dispatchEvent(new CustomEvent("animationend" + target.toString()))
       if (element.dataset.visibility == 'deleted') element.remove();
-    }, duration);
+    }, duration + delay);
   }
 }
