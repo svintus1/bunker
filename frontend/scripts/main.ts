b0d936e0a8d9023b2b104503813fdb2bc016c589
cringen33r <cringen33r@yandex.ru>
2025-04-21 15:38:59 +0300

    feat(sound): add music and music-toggle

--- frontend/scripts/main.ts
+++ frontend/scripts/main.ts
@@ -11,12 +11,29 @@ class MainMenu {
     public cover: HTMLDivElement;
     public introAudio: HTMLAudioElement | null = null;
     public introVideo: HTMLVideoElement | null = null;
+    public ambientMusic: HTMLAudioElement;
+    public musicStatus: HTMLButtonElement;
     constructor() {
         this.root = document.getElementById('root') as HTMLDivElement;
         this.body = document.body as HTMLBodyElement;
         this.splashscreen = document.getElementById('splashscreen') as HTMLDivElement;
         this.playButton = document.getElementById('play-button') as HTMLButtonElement;
         this.cover = document.getElementById('cover') as HTMLDivElement;
+        this.ambientMusic = document.getElementById('ambient-music') as HTMLAudioElement;
+        this.ambientMusic.src = './static/End of the World Echoes.mp3';
+        this.musicStatus = document.getElementById('music-status') as HTMLButtonElement;
+        AkronymEventRouter.add(this.musicStatus, "click", () => {
+            if (this.ambientMusic.paused)
+            {
+                this.ambientMusic.play()
+                this.musicStatus.setAttribute('data-mute', "false");
+            }
+            else
+            {
+                this.ambientMusic.pause()
+                this.musicStatus.setAttribute('data-mute', "true");
+            }
+        });
 
         const navEntry = performance.getEntriesByType("navigation")[0] as PerformanceNavigationTiming;
 
@@ -33,6 +50,7 @@ class MainMenu {
         AkronymAnimator.changeVisibility(this.cover, "hidden", 'fade-out', 0);
         AkronymAnimator.changeVisibility(this.playButton, "deleted", 'fade-out', 0);
         AkronymAnimator.changeVisibility(this.splashscreen, "deleted", 'fade-out', 0);
+        this.musicStatus.setAttribute('data-mute', "true");
     }
 
     initIntro(){
@@ -46,6 +64,8 @@ class MainMenu {
 
         AkronymEventRouter.add(this.playButton, "click", this.startIntro.bind(this), true);
         AkronymEventRouter.add(this.introAudio, "ended", this.endIntro.bind(this), true);
+
+        this.musicStatus.setAttribute('data-mute', "false");
     }
 
     startIntro(): void {
@@ -55,6 +75,7 @@ class MainMenu {
     }
 
     endIntro(): void {
+        this.ambientMusic.play()
         AkronymAnimator.changeVisibility(this.cover, "hidden", 'fade-out', 0);
         AkronymAnimator.changeVisibility(this.playButton, "deleted", 'fade-out', 0);
         AkronymAnimator.changeVisibility(this.splashscreen, "deleted", 'fade-out', 3000);
