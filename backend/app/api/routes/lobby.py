370b1a22d66d095ad5edfbcf6eb56ca175d35449
Demid <myemail@foobar.com>
2025-05-10 11:07:41 +0300

    fix(api)!: join lobby return player_id, handle lobby not found
    Now /lobby/join/ also returns player_id in JSON: {"lobby": lobby_out, "player_id": player.id}. Also this commit fixes case where lobby to be joined didn't exist.

--- backend/app/api/routes/lobby.py
+++ backend/app/api/routes/lobby.py
@@ -1,5 +1,5 @@
 import uuid
-from typing import Annotated
+from typing import Annotated, Any
 from fastapi import APIRouter, Body, HTTPException
 
 from app.api.deps import LobbyServiceDep, PlayerCRUDDep, UserCRUDDep
@@ -19,20 +19,25 @@ def create_lobby(
     return lobby_out
 
 
-@router.post("/join/", description="Join existing lobby. Accepts `lobby_id` and `user_id` of user that wants to join.")
+@router.post("/join/", response_model=dict[str, Any],
+             description="Join existing lobby. Accepts `lobby_id` and `user_id` of user that wants to join.")
 def join_lobby(
     lobby_id: Annotated[str, Body()],
     user_id: Annotated[uuid.UUID, Body(title="User UUID")],
     lobby_service: LobbyServiceDep,
     player_crud: PlayerCRUDDep,
     user_crud: UserCRUDDep,
-) -> LobbyOutput:
+) -> dict[str, Any]:
     user = user_crud.get_user_by_id(user_id)
     if not user:
         raise HTTPException(status_code=400,
                              detail="The user with this ID is not found")
 
-    player = lobby_service.find_player_by_user_id(lobby_id, user_id)
+    try:
+        player = lobby_service.find_player_by_user_id(lobby_id, user_id)
+    except RuntimeError as e:
+        raise HTTPException(status_code=400,
+                            detail=str(e))
     if player:
         raise HTTPException(status_code=400,
                             detail="The user is already in this lobby")
@@ -46,4 +51,6 @@ def join_lobby(
         raise HTTPException(status_code=500,
                             detail="Failed to join lobby")
     lobby_out = LobbyOutput(**lobby.model_dump(), id=lobby.id)
-    return lobby_out
+    return {"lobby": lobby_out, "player_id": player.id}
+
+
