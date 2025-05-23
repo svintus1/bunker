7b963161481ac2b1341de216e68ced10446d9069
Demid <myemail@foobar.com>
2025-05-23 14:59:22 +0300

    test(services): add test_delete_lobby_success

--- backend/app/tests/services/test_lobby_service.py
+++ backend/app/tests/services/test_lobby_service.py
@@ -76,3 +76,18 @@ def test_leave_lobby_not_found(lobby_service, mock_lobby_crud, mock_player_crud)
     mock_player_crud.get_player.return_value = None
     updated_lobby = lobby_service.leave_lobby("lobby", "player")
     assert updated_lobby is None
+
+def test_delete_lobby_success(lobby_service, mock_lobby_crud, mock_player_crud):
+    lobby_id = "lobby-pk"
+    player_id = "player-pk"
+    creator_id = uuid.uuid4()
+    lobby = Lobby(name="l", creator_id=creator_id, status="waiting", player_ids=[player_id], pk=lobby_id)
+    player = Player(user=User(name="n", id=creator_id), lobby_id=lobby_id, pk=player_id)
+    mock_lobby_crud.get_lobby.return_value = lobby
+    mock_lobby_crud.delete_lobby.return_value = True
+    mock_player_crud.get_player.return_value = player
+    mock_player_crud.delete_player.return_value = True
+
+    success = lobby_service.delete_lobby(lobby_id)
+
+    assert success is True
