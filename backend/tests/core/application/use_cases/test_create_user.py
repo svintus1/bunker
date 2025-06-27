82dcd05b448f960823eca119817fea2019f16702
Demid <myemail@foobar.com>
2025-06-27 12:59:57 +0300

    test(create_user): use extern fixtures, add negative test

--- backend/tests/core/application/use_cases/test_create_user.py
+++ backend/tests/core/application/use_cases/test_create_user.py
@@ -1,26 +1,24 @@
 import uuid
 
 import pytest
-from unittest.mock import MagicMock, AsyncMock
 
-from src.core.domain.models import User
 from src.core.application.use_cases.create_user import CreateUser
-
-
-@pytest.fixture
-def user_repository_mock():
-    """Fixture to create a mock of the UserRepository."""
-    mock = MagicMock()
-    mock.create_user = AsyncMock()
-    return mock
 
 
 @pytest.mark.asyncio
 async def test_create_user_success(user_repository_mock):
     user_name = "John Doe"
     use_case = CreateUser(user_repository=user_repository_mock)
+
     user = await use_case.execute(name=user_name)
-    assert isinstance(user, User)
+
     assert user.name == user_name
     assert isinstance(user.id, uuid.UUID)
     user_repository_mock.create_user.assert_called_once_with(user)
+
+@pytest.mark.asyncio
+async def test_create_user_empty_name(user_repository_mock):
+    use_case = CreateUser(user_repository=user_repository_mock)
+
+    with pytest.raises(ValueError, match="User name cannot be empty"):
+        await use_case.execute(name="")
